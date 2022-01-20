fad1a695f80255b961b8a7e9af18d05877cadedf
southf09 <frankstonesouth@gmail.com>
2022-01-20 16:30:45 -0500

    Added functions that animate the button and produce a sound on click

--- game.js
+++ game.js
@@ -13,33 +13,50 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100);
 
-  let audio = new Audio("sounds/" + randomChosen + ".mp3");
-  audio.play();
+  playSound(randomChosen);
 }
 
 $(".btn").click(function (e) {
   let userChosenColor = e.target.classList[1];
   userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
 });
 
-switch (userChosenColor) {
-  case "green":
-    let greenBtn = new Audio("sounds/green.mp3");
-    greenBtn.play();
-    break;
+// switch (userChosenColor) {
+//   case "green":
+//     let greenBtn = new Audio("sounds/green.mp3");
+//     greenBtn.play();
+//     break;
 
-  case "red":
-    let redBtn = new Audio("sounds/red.mp3");
-    greenBtn.play();
-    break;
+//   case "red":
+//     let redBtn = new Audio("sounds/red.mp3");
+//     redBtn.play();
+//     break;
 
-  case "yellow":
-    let yellowBtn = new Audio("sounds/yellow.mp3");
-    yellowBtn.play();
-    break;
+//   case "yellow":
+//     let yellowBtn = new Audio("sounds/yellow.mp3");
+//     yellowBtn.play();
+//     break;
 
-  case "green":
-    let greenBtn = new Audio("sounds/green.mp3");
-    greenBtn.play();
-    break;
+//   case "blue":
+//     let blueBtn = new Audio("sounds/green.mp3");
+//     blueBtn.play();
+//     break;
+
+//   default:
+//     console.log(userChosenColor);
+// }
+
+function playSound(name) {
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(() => {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
